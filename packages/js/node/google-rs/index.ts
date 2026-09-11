63442f114e619867d51d213d415105fb8db639cb
Reggie Pierce <reggie.pierce@databricks.com>
2026-09-11 16:44:06 -0400

    chore(release): 0.6.200

--- packages/js/node/google-rs/index.ts
+++ packages/js/node/google-rs/index.ts
@@ -3,5 +3,5 @@
 // Hand edits are overwritten on the next watch; this file is read-only.
 
 export const PACKAGE_IDENTIFIER = "@dbx-tools/google-rs";
-export const PACKAGE_VERSION = "0.6.199";
+export const PACKAGE_VERSION = "0.6.200";
 export * from "./src/bindings.ts";
