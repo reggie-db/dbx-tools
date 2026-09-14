9f65d37ba4337fca5bb98db923646b0adb63a8cb
Reggie Pierce <reggie.pierce@databricks.com>
2026-09-14 17:30:43 -0400

    chore(release): 0.6.207

--- packages/js/node/core-rs/index.ts
+++ packages/js/node/core-rs/index.ts
@@ -3,5 +3,5 @@
 // Hand edits are overwritten on the next watch; this file is read-only.
 
 export const PACKAGE_IDENTIFIER = "@dbx-tools/core-rs";
-export const PACKAGE_VERSION = "0.6.206";
+export const PACKAGE_VERSION = "0.6.207";
 export * from "./src/bindings.ts";
